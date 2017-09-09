b9c78d2199105e89d7fa994729da717be9116eee
Sonny Li <sonnynomnom@gmail.com>
2017-09-09 18:24:53 -0400

    Updates

--- app.js
+++ app.js
@@ -1,41 +1,74 @@
-function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
-}
+var c = document.getElementById('canv'),
+    $ = c.getContext("2d");
+var w = c.width = window.innerWidth,
+    h = c.height = window.innerHeight;
 
-function initializeClock(id, endtime) {
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+Snowy();
 
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
-
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
+function Snowy() {
+  var snow, arr = [];
+  var num = 600, tsc = 1, sp = 1;
+  var sc = 1.3, t = 0, mv = 20, min = 1;
+    for (var i = 0; i < num; ++i) {
+      snow = new Flake();
+      snow.y = Math.random() * (h + 50);
+      snow.x = Math.random() * w;
+      snow.t = Math.random() * (Math.PI * 2);
+      snow.sz = (100 / (10 + (Math.random() * 100))) * sc;
+      snow.sp = (Math.pow(snow.sz * .8, 2) * .15) * sp;
+      snow.sp = snow.sp < min ? min : snow.sp;
+      arr.push(snow);
     }
+
+  go();
+
+  function go(){
+    window.requestAnimationFrame(go);
+
+      $.clearRect(0, 0, w, h);
+      //
+      // var img = new Image();
+      // img.src = 'night2.jpg';
+      // img.onload = function() {
+      //   var pattern = $.createPattern(img, 'repeat');
+      //   $.fillStyle = pattern;
+      //   $.fillRect(0, 0, w, h);
+      // }
+      //
+      $.fill();
+
+
+      // $.clearRect(0, 0, w, h);
+      // $.fillStyle = 'hsla(242, 95%, 3%, 1)';
+      // $.fillRect(0, 0, w, h);
+      // $.fill();
+
+        for (var i = 0; i < arr.length; ++i) {
+          f = arr[i];
+          f.t += .05;
+          f.t = f.t >= Math.PI * 2 ? 0 : f.t;
+          f.y += f.sp;
+          f.x += Math.sin(f.t * tsc) * (f.sz * .3);
+          if (f.y > h + 50) f.y = -10 - Math.random() * mv;
+          if (f.x > w + mv) f.x = - mv;
+          if (f.x < - mv) f.x = w + mv;
+          f.draw();}
+ }
+
+ function Flake() {
+   this.draw = function() {
+      this.g = $.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.sz);
+      this.g.addColorStop(0, 'hsla(255,255%,255%,1)');
+      this.g.addColorStop(1, 'hsla(255,255%,255%,0)');
+      $.moveTo(this.x, this.y);
+      $.fillStyle = this.g;
+      $.beginPath();
+      $.arc(this.x, this.y, this.sz, 0, Math.PI * 2, true);
+      $.fill();}
   }
-
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
 }
-
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-initializeClock('clockdiv', deadline);
+/*________________________________________*/
+window.addEventListener('resize', function(){
+  c.width = w = window.innerWidth;
+  c.height = h = window.innerHeight;
+}, false);
