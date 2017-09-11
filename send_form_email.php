94e240bd0185a37faf0e312f414e29a6f24d00fa
Sonny Li <sonnynomnom@gmail.com>
2017-09-11 19:27:42 -0400

    asdf

--- send_form_email.php
+++ send_form_email.php
@@ -74,13 +74,10 @@ $headers = 'From: '.$email."\r\n".
 'Reply-To: '.$email."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
-?>
 
 <!-- include your own success html here -->
 
 Thank you for contacting us. We will be in touch with you very soon.
 
-<?php
 
-}
 ?>
